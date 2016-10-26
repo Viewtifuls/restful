f29f3c279dda531d322d5191116119e515037d24
Viewtiful <viewtiful@mail.ru>
2016-10-26 15:50:38 +0300

    https fix

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ from flask import Flask, render_template, request, jsonify
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as BS
 
+
 app = Flask(__name__)
+
 
 def count_tags(url):
     protocol = 'http://'
-    if protocol not in url:
+    if url[0:4] != protocol[0:4]:
         url = protocol + url
     try:
         page = urlopen(url).read().decode()
@@ -30,15 +32,18 @@ def count_tags(url):
         count = 'Try something else'
     return count
 
+
 @app.route('/')
 def index():
     return render_template('index.html', url = request.args.get('url'))
+
 
 @app.route('/data')
 def data():
     url = request.args.get('url')
     number = count_tags(url)
     return jsonify({"url": url, "number": number})
+
 
 if __name__ == '__main__':
     app.run(port=getuid() + ADDITIVE_FOR_UID, debug=True)
